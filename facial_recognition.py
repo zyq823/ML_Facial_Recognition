bdca0b57784102507f362bb7329d7442e2761cfb
yiqingzhao <yiqingzhao@tamu.edu>
2020-10-15 03:32:27 -0500

    added data preprocessing

--- facial_recognition.py
+++ facial_recognition.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
+import numpy as np
+import statistics as stats
+import math
 
 # initialization for Question 2
 
@@ -7,20 +9,36 @@ class DataPoint(object):
         self.emotion = cols['emotion']
         self.pixels = cols['pixels']
 
-    def __str__(self):
-        return "emotion: {}".format(self.emotion)
+
+# Normalize the images
+def preprocessing(dataset):
+    for i, image in enumerate(dataset):
+        mean = stats.mean(image.pixels)
+        stddev = np.std(image.pixels)
+        adjusted_stddev = max(stddev, 1.0/math.sqrt(2304))
+        print(image.pixels[0])
+        for k, p in enumerate(image.pixels):
+            image.pixels[k] = (p - mean) / adjusted_stddev
+        print(image.pixels[0])
+
+
+    return dataset
 
 def parse_dataset(filename):
     data_file = open(filename, 'r')  # Open File "to read"
     dataset = []  # List to hold Datapoint objects
     # emotion_class = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Sad', 5: 'Surprise', 6: 'Neutral'}
 
-    for image in data_file:
-        emotion, pixels = image.strip().split(',')  # strip() removes '\n', and split(',') splits the line at tabs
-        # print(emotion)
+    for i, image in enumerate(data_file):
+        if i == 0:
+            continue # ignore headers
+        emotion, pixelStr = image.strip().split(',')  # strip() removes '\n', and split(',') splits the line at tabs
+        pixel_list = pixelStr.split()
+        map_pixel = map(float, pixel_list)
+        pixels = list(map_pixel)
         dataset.append(DataPoint({'emotion': emotion, 'pixels': pixels}))
         
-    print(len(dataset))
+    preprocessing(dataset)
     print("Number of samples by emotion: Angry - {0} , Disgust - {1} , Fear - {2} , Happy - {3} , Sad - {4} , Surprise - {5} , Neutral - {6}".format(len([i for i in dataset if i.emotion == '0']), len([i for i in dataset if i.emotion == '1']), len([i for i in dataset if i.emotion == '2']), len([i for i in dataset if i.emotion == '3']), len([i for i in dataset if i.emotion == '4']), len([i for i in dataset if i.emotion == '5']), len([i for i in dataset if i.emotion == '6'])))
     return dataset
 
